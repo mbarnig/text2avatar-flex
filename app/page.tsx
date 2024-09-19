90cc6001ad549de7d8fb77bc09f3a8054d8221a6
Marco BARNIG <mbarnig@pt.lu>
2024-09-19 13:55:55 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,7 @@ import InteractiveAvatar from "@/components/InteractiveAvatarTextInput";
 export default function App() {
   return (
      <div>
-       <div>
-          <InteractiveAvatarTextInput />
-       </div>
+        <InteractiveAvatarTextInput />
      </div>
   );
 } 
